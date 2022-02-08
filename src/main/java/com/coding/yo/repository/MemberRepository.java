1e728023c7419c8741f9e5426c3eede3e4be1a5c
jieun-dev1 <jkdev1324@gmail.com>
2022-02-08 19:57:43 +0900

    FEAT:  Member 타입에서 UserDetails 구현체로 변경

--- src/main/java/com/coding/yo/repository/MemberRepository.java
+++ src/main/java/com/coding/yo/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package com.coding.yo.repository;
 
 import com.coding.yo.entity.Member;
+import com.coding.yo.security.UserDetailsImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member,String> {
+public interface MemberRepository extends JpaRepository<UserDetailsImpl,String> {
 }
