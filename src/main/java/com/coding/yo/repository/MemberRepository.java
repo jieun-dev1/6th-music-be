1d8c0ce8b0f6c4cc14cdb51589c2fe854ab3f281
jieun-dev1 <jkdev1324@gmail.com>
2022-02-10 13:29:51 +0900

    Chore: 구조 변경

--- src/main/java/com/coding/yo/repository/MemberRepository.java
+++ src/main/java/com/coding/yo/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.coding.yo.repository;
 
-import com.coding.yo.entity.Member;
-import com.coding.yo.security.UserDetailsImpl;
+import com.coding.yo.security.entity.UserDetailsImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
