fa17f73d8c5874a74be14a2cde8f848aabfbc70f
jieun-dev1 <jkdev1324@gmail.com>
2022-02-10 13:05:38 +0900

    FEAT: profileUrl MemberInfo에 추가

--- src/main/java/com/coding/yo/security/message/response/MemberInfo.java
+++ src/main/java/com/coding/yo/security/message/response/MemberInfo.java
@@ -8,10 +8,13 @@ public class MemberInfo {
     private String uid;
     private String email;
     private String nickname;
+    private String profileUrl;
 
     public MemberInfo(UserDetailsImpl userDetailsImpl) {
         this.uid = userDetailsImpl.getUsername();
         this.email = userDetailsImpl.getEmail();
         this.nickname = userDetailsImpl.getNickname();
+        this.profileUrl = userDetailsImpl.getProfileUrl();
+
     }
 }
