ee14280022aacc2d09308af505322d1bec147ec3
jieun-dev1 <jkdev1324@gmail.com>
2022-02-13 01:03:55 +0900

    CHORE: 로그 추가

--- src/main/java/com/coding/yo/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/coding/yo/security/service/UserDetailsServiceImpl.java
@@ -5,11 +5,14 @@ import com.coding.yo.repository.MemberRepository;
 import com.google.firebase.auth.FirebaseAuth;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -20,12 +23,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String uid) throws UsernameNotFoundException {
+
         log.info("loadUserByUsername 진입");
         UserDetails userDetails = memberRepository.findByUid(uid).
                 orElseThrow(() -> new UsernameNotFoundException("Can't find " + uid));
 
         return userDetails;
     }
+
 
     @Transactional
     public Member register(String uid, String email, String username, String profileUrl) {
@@ -35,8 +40,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 .username(username)
                 .profileUrl(profileUrl)
                 .build();
+        log.info("member builder");
 
         memberRepository.save(member);
+        log.info("member 저장");
+
         return member;
     }
 }
