8c6cdab67f8798ea012fb42aa19306b5a93184ac
jieun-dev1 <jkdev1324@gmail.com>
2022-02-22 01:01:23 +0900

    FEAT: 로컬 회원 가입 용 DTO 추가

---
+++ src/main/java/com/coding/yo/security/message/response/LocalMemberSignupRequestDto.java
@@ -0,0 +1,26 @@
+package com.coding.yo.security.message.response;
+
+
+import com.coding.yo.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocalMemberSignupRequestDto {
+    private String uid;
+    private String email;
+    private String username;
+    private String profileUrl;
+
+
+    public LocalMemberSignupRequestDto(Member member) {
+        this.uid = member.getUid();
+        this.email = member.getEmail();
+        this.username = member.getUsername();
+        this.profileUrl = member.getProfileUrl();
+    }
+}
+
