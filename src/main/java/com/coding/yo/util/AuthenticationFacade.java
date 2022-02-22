c89f2c340b1ccdb9b705e0d354de9ea15904ae48
jieun-dev1 <jkdev1324@gmail.com>
2022-02-23 02:01:04 +0900

    FEAT: Authentication Interface

--- src/main/java/com/coding/yo/util/AuthenticationFacade.java
+++ src/main/java/com/coding/yo/util/AuthenticationFacade.java
@@ -1,4 +1,11 @@
 package com.coding.yo.util;
 
-public class AuthenticationFacade {
+import org.springframework.security.core.Authentication;
+
+/**
+ * Custom Interface 통해 user 가져오는 방법
+ * (1) DI 를 완전히 활용하고 (2) AUTHENTICATION 을 어디서든 불러오기 위해서 (@CONTROLLER BEAN 포함) FACADE STATIC ACCESS 를 숨기는 것이다.
+ */
+public interface AuthenticationFacade {
+    Authentication getAuthentication();
 }
