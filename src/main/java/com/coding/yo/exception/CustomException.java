04e2e43b9dfe7de0b489d7319615a6afb85923e1
jieun-dev1 <jkdev1324@gmail.com>
2022-02-22 20:58:35 +0900

    CHORE: Firebase 잘못 import 된 부분 수정

--- src/main/java/com/coding/yo/exception/CustomException.java
+++ src/main/java/com/coding/yo/exception/CustomException.java
@@ -1,12 +1,13 @@
 package com.coding.yo.exception;
 
-import com.google.firebase.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
+
     public CustomException(ErrorCode errorCode) { this.errorCode = errorCode; }
+
     public CustomException(ErrorCode errorCode, String message) {
          super(message);
         this.errorCode = errorCode;
