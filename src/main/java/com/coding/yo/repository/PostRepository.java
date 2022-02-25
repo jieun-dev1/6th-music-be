7b79183160148955631e56c2ad4fd43d59a86259
jieun-dev1 <jkdev1324@gmail.com>
2022-02-26 00:15:29 +0900

    CHORE: @Repository롬복 추가

--- src/main/java/com/coding/yo/repository/PostRepository.java
+++ src/main/java/com/coding/yo/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.coding.yo.repository;
 
 import com.coding.yo.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
