b173a120ea9db6e0035150239ccb726aaf082053
jieun-dev1 <jkdev1324@gmail.com>
2022-02-27 00:50:39 +0900

    FEAT: FirebaseConfig 를 환경변수jar로 변경 (파일을 노출시키지 않기 위함)

--- src/main/java/com/coding/yo/security/config/FirebaseConfig.java
+++ src/main/java/com/coding/yo/security/config/FirebaseConfig.java
@@ -4,12 +4,15 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 public class FirebaseConfig {
     /**
@@ -18,19 +21,22 @@ public class FirebaseConfig {
      * ClassPathResource 로 파일 경로 찾고 읽어옴 (getInputStream)
      * GoogleCredentials.fromStream: JSON 포맷의 구글 Credential 가져옴
      * FirebaseApp 은 FirebaseSDK가 진입할 때 거치는 포인트고 FirebaseAPI 는 이 앱을 사용하는 것이 default. 따라서 초기화 필요.
-     *
+     *https://firebase.google.com/docs/database/admin/start?hl=ko
      * To do: KEY 없을 때의 예외처리 코드 필요.
+     *
      */
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("./firebase.json");
 
-        ClassPathResource key = new ClassPathResource("serviceAccountKey.json");
-
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(key.getInputStream()))
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
-        FirebaseApp.initializeApp(options);
-        return FirebaseAuth.getInstance(FirebaseApp.getInstance());
+
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+        log.info("FirebaseApp initialized" + app.getName());
+        return FirebaseAuth.getInstance(app.getInstance()); //returns The FirebaseAuth instance for the default FirebaseApp.
     }
 
 }
