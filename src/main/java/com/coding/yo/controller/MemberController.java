78f931c7504245a12ce1b1b4eed8d73eec794eec
jieun-dev1 <jkdev1324@gmail.com>
2022-03-01 20:48:55 +0900

    FEAT: FirebaseAuth 프론트 필요한 부분 주석처리

--- src/main/java/com/coding/yo/controller/MemberController.java
+++ src/main/java/com/coding/yo/controller/MemberController.java
@@ -1,9 +1,9 @@
 package com.coding.yo.controller;
 
 import com.coding.yo.entity.Member;
-import com.coding.yo.security.message.request.LocalMemberSignupRequestDto;
-import com.coding.yo.security.message.request.MemberSignupRequestDto;
-import com.coding.yo.security.service.UserDetailsServiceImpl;
+import com.coding.yo.message.request.LocalMemberSignupRequestDto;
+import com.coding.yo.message.request.MemberSignupRequestDto;
+import com.coding.yo.service.UserDetailsServiceImpl;
 import com.coding.yo.util.RequestUtil;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/users")
 public class MemberController {
-    private final FirebaseAuth firebaseAuth;
+//    private final FirebaseAuth firebaseAuth;
     private final UserDetailsServiceImpl userDetailsServiceImpl;
 
     /**
@@ -43,26 +43,26 @@ public class MemberController {
     }
 
     //회원가입
-    @PostMapping("")
-    public ResponseEntity<MemberSignupRequestDto> register(@RequestHeader("Authorization") String authorization) {
-
-        //Token 가져온다
-        FirebaseToken decodedToken;
-        try {
-            String token = RequestUtil.getAuthorizationToken(authorization);
-            decodedToken = firebaseAuth.verifyIdToken(token);
-
-        } catch (IllegalArgumentException | FirebaseAuthException e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
-                    "{\"code\":\"INVALID_TOKEN\", \"message\":\"" + e.getMessage() + "\"}");
-        }
-        log.info("controller 회원가입 decoded: {} {} {} {}", decodedToken.getUid(), decodedToken.getEmail(), decodedToken.getName(), decodedToken.getPicture());
-        //사용자를 등록한다.
-        Member registeredUser = userDetailsServiceImpl.register(decodedToken.getUid(), decodedToken.getEmail(), decodedToken.getName(), decodedToken.getPicture());
-        log.info("userDetailsServiceImpl 등록");
-        return new ResponseEntity<>(new MemberSignupRequestDto(registeredUser), HttpStatus.CREATED);
-//        return new MemberInfo;
-    }
+//    @PostMapping("")
+//    public ResponseEntity<MemberSignupRequestDto> register(@RequestHeader("Authorization") String authorization) {
+//
+//        //Token 가져온다
+//        FirebaseToken decodedToken;
+//        try {
+//            String token = RequestUtil.getAuthorizationToken(authorization);
+//            decodedToken = firebaseAuth.verifyIdToken(token);
+//
+//        } catch (IllegalArgumentException | FirebaseAuthException e) {
+//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+//                    "{\"code\":\"INVALID_TOKEN\", \"message\":\"" + e.getMessage() + "\"}");
+//        }
+//        log.info("controller 회원가입 decoded: {} {} {} {}", decodedToken.getUid(), decodedToken.getEmail(), decodedToken.getName(), decodedToken.getPicture());
+//        //사용자를 등록한다.
+//        Member registeredUser = userDetailsServiceImpl.register(decodedToken.getUid(), decodedToken.getEmail(), decodedToken.getName(), decodedToken.getPicture());
+//        log.info("userDetailsServiceImpl 등록");
+//        return new ResponseEntity<>(new MemberSignupRequestDto(registeredUser), HttpStatus.CREATED);
+////        return new MemberInfo;
+//    }
 
     //로그인
     @GetMapping("/me")
