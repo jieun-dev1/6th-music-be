e07f480269948ba83ab5cfae6981cd39808d8742
jieun-dev1 <jkdev1324@gmail.com>
2022-03-01 20:45:46 +0900

    FEAT: Comment 로직 추가

--- src/main/java/com/coding/yo/service/post/PostService.java
+++ src/main/java/com/coding/yo/service/post/PostService.java
@@ -1,10 +1,11 @@
-package com.coding.yo.security.service.post;
+package com.coding.yo.service.post;
 
 import com.coding.yo.entity.Member;
 import com.coding.yo.entity.Post;
-import com.coding.yo.repository.MemberRepository;
-import com.coding.yo.security.message.request.PostDto;
-import com.coding.yo.security.message.response.PostResponseDto;
+import com.coding.yo.message.request.CommentRequestDto;
+import com.coding.yo.message.request.PostDto;
+import com.coding.yo.message.response.CommentResponseDto;
+import com.coding.yo.message.response.PostResponseDto;
 import org.springframework.data.domain.Page;
 
 public interface PostService {
@@ -13,7 +14,8 @@ public interface PostService {
     void toCreatePost(PostDto postDto, Member member);
     void toEditPost(PostDto postDto, Member member);
     void toDeletePost(PostDto postDto, Member member);
+    void toCreateComment(CommentRequestDto commentRequestDto, Member member, Long postId);
+
+
 }
 
-//    void toCreateComment(CommentRequestDto commentRequestDto, Member member, Post post
-//    )
