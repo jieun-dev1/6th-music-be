48e879645d8ddafb84dd1686d54b73cc778d587a
jieun-dev1 <jkdev1324@gmail.com>
2022-03-01 20:47:34 +0900

    FEAT: ID찾는 로직 추가

--- src/main/java/com/coding/yo/repository/PostRepository.java
+++ src/main/java/com/coding/yo/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.coding.yo.repository;
 
+import com.coding.yo.entity.Member;
 import com.coding.yo.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+//    Optional<Post> findById(Long postId);
+        Optional<Post> findById(Long id);
+
 }
