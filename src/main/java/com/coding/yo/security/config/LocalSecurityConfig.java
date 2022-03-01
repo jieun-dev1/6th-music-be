7093b9b1d541cc833d1368839d70aba14dbcdc59
jieun-dev1 <jkdev1324@gmail.com>
2022-03-01 13:07:40 +0900

    FEAT: LocalSecurityConfig 주석처리 (프론트 개발시 사용)

--- src/main/java/com/coding/yo/security/config/LocalSecurityConfig.java
+++ src/main/java/com/coding/yo/security/config/LocalSecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Profile("local")
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
